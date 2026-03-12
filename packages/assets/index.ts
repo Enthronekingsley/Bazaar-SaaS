b297b33af31d62f8abcd9d72d4ca3a75051d9892
Kingsley Enthrone Keneigwe <kingsleykeneigwe@gmail.com>
2026-03-12 14:41:29 +0100

    feat: initialize bazaar turborepo monorepo

---
+++ packages/assets/index.ts
@@ -0,0 +1,10 @@
+export { default as BazaarIconBlack } from "./logos/bazaar_icon_black.png";
+export { default as BazaarIconBlue } from "./logos/bazaar_icon_blue.png";
+export { default as BazaarIcon } from "./logos/bazaar_icon.png";
+
+export { default as BazaarLogoBlack } from "./logos/bazaar_logo_black.png";
+export { default as BazaarLogoBlue } from "./logos/bazaar_logo_blue.png";
+export { default as BazaarLogo } from "./logos/bazaar_logo.png";
+
+export { default as FlutterwavePng } from "./logos/flutterwave.png";
+export { default as Opay } from "./logos/opay.png";
